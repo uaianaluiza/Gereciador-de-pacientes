206071dab96a0d6b1f5d9c041d1ca5f7db2f74ae
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-03-08 16:06:19 -0300

    Adicionar resposta de erro ao registro de hostorico

---
+++ src/main/java/br/com/zup/App/controllers/HistoricoService.java
@@ -0,0 +1,25 @@
+package br.com.zup.App.controllers;
+
+import br.com.zup.App.models.Historico;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.server.ResponseStatusException;
+
+public class HistoricoService {
+
+    @Autowired
+    private HistoricoService historicoService;
+
+    @PostMapping("{cpf}/")
+    public Historico registrarHistorico(@RequestBody Historico historico, @PathVariable String cpf){
+        try {
+            Historico objHistorico = historicoService.registrarHistorico(historico,cpf);
+            return objHistorico;
+        }catch (RuntimeException error){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, error.getMessage());
+        }
+    }
+}
