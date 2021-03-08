4d3225b97be36eeeedfc5c4e73142a0053c90042
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-03-08 14:33:41 -0300

    Criar método para cadastro de paciente

--- src/main/java/br/com/zup/App/controllers/PacienteController.java
+++ src/main/java/br/com/zup/App/controllers/PacienteController.java
@@ -1,12 +1,24 @@
 package br.com.zup.App.controllers;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import br.com.zup.App.models.Paciente;
+import br.com.zup.App.services.PacienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("pacientes/")
 
-
 public class PacienteController {
+
+    @Autowired
+    private PacienteService pacienteService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Paciente cadastrarPaciente(@RequestBody Paciente paciente){
+        Paciente objetoPaciente = pacienteService.salvarPaciente(paciente);
+        return paciente;
+    }
 }
