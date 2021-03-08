6a34b485913d3157e65a0ebb9d9db41a1ce5b687
Ana Luiza Santos <71670666+uaianaluiza@users.noreply.github.com>
2021-03-08 15:05:01 -0300

    Adicionar DTO para cadastro de paciente

--- src/main/java/br/com/zup/App/controllers/PacienteController.java
+++ src/main/java/br/com/zup/App/controllers/PacienteController.java
@@ -6,6 +6,7 @@ import br.com.zup.App.services.PacienteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("pacientes/")
@@ -20,5 +21,14 @@ public class PacienteController {
     public Paciente cadastrarPaciente(@RequestBody Paciente paciente){
         Paciente objetoPaciente = pacienteService.salvarPaciente(paciente);
         return paciente;
+    }
+    @GetMapping("{cpf}/")
+    public Paciente pesquisarPaciente(@PathVariable String cpf){
+        try {
+            Paciente paciente = pacienteService.pesquiserPacientePeloCPF(cpf);
+            return paciente;
+        }catch (RuntimeException erro){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, erro.getMessage());
+        }
     }
 }
